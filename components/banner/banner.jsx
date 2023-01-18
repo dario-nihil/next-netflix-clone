03064c909a9ddc3f87fc859386f970a3f92f21b4
dario-nihil <dario.nihil@gmail.com>
2023-01-18 14:37:52 +0100

    implemeted styles for the Banner component

--- components/banner/banner.jsx
+++ components/banner/banner.jsx
@@ -6,18 +6,26 @@ const Banner = ({ title, subTitle, imgUrl }) => {
   };
 
   return (
-    <div>
-      <h3>{title}</h3>
-      <h3>{subTitle}</h3>
-      <button onClick={handleOnPlay}>Play</button>
+    <div className={styles.container}>
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+          <div className={styles.nseriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>S E R I E S</p>
+          </div>
+          <h3 className={styles.title}>{title}</h3>
+          <h3 className={styles.subTitle}>{subTitle}</h3>
+          <div className={styles.playBtnWrapper}>
+            <button className={styles.btnWithIcon} onClick={handleOnPlay}>
+              <span className={styles.playText}>Play</span>
+            </button>
+          </div>
+        </div>
+      </div>
       <div
+        className={styles.bannerImg}
         style={{
           backgroundImage: `url(${imgUrl})`,
-          width: "100%",
-          height: "100%",
-          position: "absolute",
-          backgroundSize: "cover",
-          backgroundPosition: "50% 50%",
         }}
       ></div>
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { Roboto_Slab } from "@next/font/google";
 import "@/styles/globals.css";
 
-const robotoSlab = Roboto_Slab({ subsets: ["latin"] });
+const robotoSlab = Roboto_Slab({ weight: ["700"], subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (

9620bdbff197ea595cc6f77f31bdbfeeb310242b
dario-nihil <dario.nihil@gmail.com>
2023-01-18 13:44:49 +0100

    first commit, added global styles and cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import { Roboto_Slab } from "@next/font/google";
+import "@/styles/globals.css";
+
+const robotoSlab = Roboto_Slab({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          html {
+            font-family: ${robotoSlab.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }

2db65f5a2208c68a13a5f3c7f5454133e2a67e8e
dario-nihil <dario.nihil@gmail.com>
2023-01-18 15:49:28 +0100

    added styles for NavBar Component

--- components/nav/navbar.jsx
+++ components/nav/navbar.jsx
@@ -1,23 +1,36 @@
+import Link from "next/link";
 import styles from "./navbar.module.css";
 
 const NavBar = ({ userName }) => {
   return (
-    <div>
-      The NavBar Component<p>{userName}</p>
-      <ul>
-        <li>Home</li>
-        <li>My List</li>
-      </ul>
-      <nav>
-        <div>
-          <button>
-            <p>{userName}</p>
-          </button>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <Link className={styles.logoLink} href="/">
+          <div className={styles.logoWrapper}>Netflix</div>
+        </Link>
+
+        <ul className={styles.navItems}>
+          <li className={styles.navItem}>Home</li>
+          <li className={styles.navItem2}>My List</li>
+        </ul>
+        <nav className={styles.navContainer}>
           <div>
-            <a>Sign out</a>
+            <button className={styles.userNameBtn}>
+              <p className={styles.userName}>{userName}</p>
+              {/** expand more icon */}
+            </button>
+
+            <div className={styles.navDropdown}>
+              <div>
+                <Link className={styles.linkName} href="">
+                  Sign out
+                </Link>
+                <div className={styles.lineWrapper}></div>
+              </div>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </div>
   );
 };

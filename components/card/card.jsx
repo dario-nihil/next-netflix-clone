618ae0c2edc59c27b5c638cfe0b0c8fb7a54637a
dario-nihil <dario.nihil@gmail.com>
2023-01-19 09:23:33 +0100

    handling error handling in loading images in Card component

--- components/card/card.jsx
+++ components/card/card.jsx
@@ -1,11 +1,20 @@
 import Image from "next/image";
+import { useState } from "react";
 import styles from "./card.module.css";
 
 const Card = ({ imgUrl, size }) => {
+  const [imgSrc, setImgSrc] = useState(imgUrl);
+
   const classMap = {
     large: styles.lgItem,
     medium: styles.mdItem,
     small: styles.smItem,
+  };
+
+  const handleOnError = () => {
+    setImgSrc(
+      "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1159&q=80"
+    );
   };
 
   return (
@@ -13,7 +22,8 @@ const Card = ({ imgUrl, size }) => {
       The Card Component
       <div className={classMap[size]}>
         <Image
-          src={imgUrl}
+          onError={handleOnError}
+          src={imgSrc}
           alt="image"
           className={styles.cardImg}
           fill={true}

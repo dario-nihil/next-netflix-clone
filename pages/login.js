cab960337bef6ca5f14113749ff7711b25ef2046
dario-nihil <dario.nihil@gmail.com>
2023-01-20 09:17:47 +0100

    implemented styles for Login page

--- pages/login.js
+++ pages/login.js
@@ -5,12 +5,17 @@ import Image from "next/image";
 import styles from "../styles/Login.module.css";
 
 const Login = () => {
+  const handleLoginWithEmail = (e) => {
+    e.preventDefault();
+    console.log("hi button");
+  };
+
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Netfix SignIn</title>
       </Head>
-      <header>
+      <header className={styles.header}>
         <div className={styles.headerWrapper}>
           <Link className={styles.logoLink} href="/">
             <div className={styles.logoWrapper}>
@@ -24,7 +29,21 @@ const Login = () => {
           </Link>
         </div>
       </header>
-    </>
+      <main className={styles.main}>
+        <div className={styles.mainWrapper}>
+          <h1 className={styles.signinHeader}>Sign In</h1>
+          <input
+            type="text"
+            placeholder="Email address"
+            className={styles.Input}
+          />
+          <p className={styles.userMsg}></p>
+          <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
+            Sign In
+          </button>
+        </div>
+      </main>
+    </div>
   );
 };
 

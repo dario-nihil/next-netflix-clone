8b45e4eed20c1d5cfc36eb852c4f56fbf670b37d
dario-nihil <dario.nihil@gmail.com>
2023-01-20 16:03:07 +0100

    fix check login route protection

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { magic } from "../lib/magic-client";
 import { Roboto_Slab } from "@next/font/google";
@@ -8,30 +8,52 @@ const robotoSlab = Roboto_Slab({ weight: ["700"], subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const checkLogIn = async () => {
       const isLoggedIn = await magic.user.isLoggedIn();
 
       if (isLoggedIn) {
-        router.push("/");
+        setIsLoading(false);
       } else {
-        router.push("/login");
+        setIsLoading(false);
       }
     };
 
     checkLogIn();
   }, []);
-  return (
-    <>
-      <style jsx global>
-        {`
-          html {
-            font-family: ${robotoSlab.style.fontFamily};
-          }
-        `}
-      </style>
-      <Component {...pageProps} />
-    </>
-  );
+
+  useEffect(() => {
+    const handleRouteChangeComplete = () => {
+      setIsLoading(false);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+    router.events.on("routeChangeError", handleRouteChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+      router.events.off("routeChangeError", handleRouteChangeComplete);
+    };
+  }, [router]);
+
+  let contentDisplayed = <div>Loading...</div>;
+
+  if (!isLoading) {
+    contentDisplayed = (
+      <>
+        <style jsx global>
+          {`
+            html {
+              font-family: ${robotoSlab.style.fontFamily};
+            }
+          `}
+        </style>
+        <Component {...pageProps} />
+      </>
+    );
+  }
+
+  return contentDisplayed;
 }

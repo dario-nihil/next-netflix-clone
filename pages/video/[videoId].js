ca253f45152e90def496a6ff3f45d4af71215bc0
dario-nihil <dario.nihil@gmail.com>
2023-01-23 11:47:28 +0100

    implenenting ISR for Video page

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,13 +1,44 @@
 import { useRouter } from "next/router";
 import Modal from "react-modal";
+import cls from "classnames";
 
 import styles from "../../styles/Video.module.css";
+import { NextFetchEvent } from "next/server";
 
 Modal.setAppElement("#__next");
 
-const Video = () => {
+export const getStaticProps = async () => {
+  const video = {
+    title: "Hi cute dog",
+    publishTime: "1990-01-01",
+    description:
+      "A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger?",
+    channelTitle: "Paramaount Pictures",
+    viewCount: 10000,
+  };
+
+  return {
+    props: { video },
+    revalidate: 10,
+  };
+};
+
+export const getStaticPaths = async () => {
+  const listOfVideos = ["mYfJxlgR2jw", "4zH5iYM4wJo", "KCPEHsAViiQ"];
+
+  const paths = listOfVideos.map((videoId) => ({ params: { videoId } }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+const Video = ({ video }) => {
   const router = useRouter();
   const { videoId } = router.query;
+
+  const { channelTitle, description, publishTime, title, viewCount } = video;
 
   return (
     <div className={styles.container}>
@@ -29,6 +60,25 @@ const Video = () => {
           src={`http://www.youtube.com/embed/${videoId}?autoplay=1&origin=http://example.com&controls=0&rel=0`}
           frameborder="0"
         />
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={cls(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={cls(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </Modal>
     </div>
   );

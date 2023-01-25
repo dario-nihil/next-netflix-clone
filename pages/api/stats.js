6605e1604998abb19cb35542241398b80f5fbc1c
dario-nihil <dario.nihil@gmail.com>
2023-01-25 11:12:00 +0100

    removed litaral values inside stats api and cleanup code

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -5,21 +5,19 @@ const stats = async (req, res) => {
   if (req.method === "POST") {
     try {
       const { token } = req.cookies;
+      const videoId = req.query.videoId;
+
       if (!token) {
         return res.status(403).json({ message: "Unauthorized" });
       }
 
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      console.log({ decoded });
-      const userId = "did:ethr:0x1B82eEb77f0693a3D336AB8557B94FE5E6f28c9F";
-      const videoId = "bKh2G73gCCs";
-
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+      const userId = decodedToken.issuer;
       const findedVideoId = await findVideoIdByUser(token, userId, videoId);
 
-      console.log({ findedVideoId });
-
-      return res.status(200).json({ message: "Done", decoded, findedVideoId });
+      return res
+        .status(200)
+        .json({ message: "Done", decodedToken, findedVideoId });
     } catch (error) {
       console.log("Error occurred /stats", error);
       return res

809ed5e0b2b3adcceac10db684b4d844ea44ce8b
dario-nihil <dario.nihil@gmail.com>
2023-01-25 10:10:09 +0100

    extracted cookie from req in stats api, and implemented a basic authorization check

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,6 +1,19 @@
 const stats = async (req, res) => {
   if (req.method === "POST") {
-    res.status(200).json({ message: "Done" });
+    const { token } = req.cookies;
+
+    try {
+      if (!token) {
+        return res.status(403).json({ message: "Unauthorized" });
+      }
+
+      return res.status(200).json({ message: "Done" });
+    } catch (error) {
+      console.log("Error occurred /stats", error);
+      res
+        .status(500)
+        .json({ message: "Something went wrong", error: error.messge });
+    }
   }
 
   res.status(403).json({ message: "Error" });

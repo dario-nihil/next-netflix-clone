0eefe470e935bb60b4d79ba9ade10ff3e8d6d485
dario-nihil <dario.nihil@gmail.com>
2023-01-26 11:40:41 +0100

    implemented display watched videos in Home, and fetched high resolution thumbnails

--- pages/index.js
+++ pages/index.js
@@ -16,14 +16,19 @@ export const getServerSideProps = async () => {
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3N1ZXIiOiJkaWQ6ZXRocjoweDFCODJlRWI3N2YwNjkzYTNEMzM2QUI4NTU3Qjk0RkU1RTZmMjhjOUYiLCJwdWJsaWNBZGRyZXNzIjoiMHgxQjgyZUViNzdmMDY5M2EzRDMzNkFCODU1N0I5NEZFNUU2ZjI4YzlGIiwiZW1haWwiOiJkYXJpby5uaWhpbEBnbWFpbC5jb20iLCJvYXV0aFByb3ZpZGVyIjpudWxsLCJwaG9uZU51bWJlciI6bnVsbCwid2FsbGV0cyI6W10sImlhdCI6MTY3NDU3MjQzMCwiZXhwIjoxNjc1MTc3MjMwLCJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsidXNlciIsImFkbWluIl0sIngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InVzZXIiLCJ4LWhhc3VyYS11c2VyLWlkIjoiZGlkOmV0aHI6MHgxQjgyZUViNzdmMDY5M2EzRDMzNkFCODU1N0I5NEZFNUU2ZjI4YzlGIn19.R0iEdDMq-vv9aqnmb3ZS8A3CoH9sum24yXkvmdToAbk";
 
   const watchItAgainVideos = await getWatchItAgainVideos(token, userId);
-  console.log({ watchItAgainVideos });
   const disneyVideos = await getVideos("disney trailer");
   const productivityVideos = await getVideos("productivity trailer");
   const travelVideos = await getVideos("travel trailer");
   const popularVideos = await getPopularVideos();
 
   return {
-    props: { disneyVideos, travelVideos, productivityVideos, popularVideos },
+    props: {
+      disneyVideos,
+      travelVideos,
+      productivityVideos,
+      popularVideos,
+      watchItAgainVideos,
+    },
   };
 };
 
@@ -32,6 +37,7 @@ export default function Home({
   travelVideos,
   productivityVideos,
   popularVideos,
+  watchItAgainVideos,
 }) {
   return (
     <div className={styles.container}>
@@ -54,6 +60,11 @@ export default function Home({
         />
         <div className={styles.sectionWrapper}>
           <SectionCards title="Disney" videos={disneyVideos} size="large" />
+          <SectionCards
+            title="Watch it again"
+            videos={watchItAgainVideos}
+            size="small"
+          />
           <SectionCards title="Travel" videos={travelVideos} size="small" />
           <SectionCards
             title="Productivity"
